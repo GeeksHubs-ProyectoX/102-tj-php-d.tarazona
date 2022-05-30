d325c6b9a50548311019f0114113926d653ecc4d
Dani <d.tarazoma@geekshubs.com>
2022-05-30 16:48:11 +0200

    GREEN: numeros primos done

--- src/Numbers.php
+++ src/Numbers.php
@@ -5,6 +5,38 @@ class Numbers
 {
     public function apply($input)
     {
-        return null;
+        if($input === 1) {
+            return -1;
+        }
+
+        if($input === 2) {
+            return [2];
+        }
+
+        if($input === 3) {
+            return [2,3];
+        }
+
+        $contador = 0;
+        // $k = 0;
+        $numerosPrimos = [];
+        for ($i=1; $i<$input ; $i++) { 
+            for ($j=$i; $j > 0 ; $j--) { 
+                if($i%$j == 0) {
+                    $contador++;
+                }
+            }
+
+            if($contador<=2){
+                // $k++;
+                if ($i != 1) {
+                    $numerosPrimos[] += $i;
+                }                
+            }
+
+            $contador = 0;
+        }
+
+        return $numerosPrimos;
     }
 }
